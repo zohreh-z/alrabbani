2e7c95b9590300f4a3c5f94b30747b04f848fc06
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-28 16:12:24 +0330

    add some file....

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title></title>
+	<link href="1styles.css" rel="stylesheet" type="text/css" />
+	<link href="style.less" rel="stylesheet/less" type="text/css" />
+	<script type="text/javascript" src="scripts/less.js">
+	</script>
+	<!--[if IE]>
+		<script type="text/javascript" src="/scripts/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	
+</body>
+</html>
