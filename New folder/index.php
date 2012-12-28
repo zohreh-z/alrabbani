f0377a2f02e4a53ddd11517116320fb2c5c51a68
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-28 13:01:36 +0330

    add index.php & style.css

---
+++ New folder/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div class="container">
+		<div class="top">
+			<div class="allah"></div>
+			<div class="mo"></div>
+		</div>
+		<div class="end"></div>
+	</div>
+	
+	
+</body>
+</html>
