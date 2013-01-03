fae62cf275d135a7d2b1246b9b8a138aa36ee9c0
zohreh-z <zahedi.zohreh@gmail.com>
2013-01-03 10:38:56 +0330

    some change....

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8" />
-	<title></title>
+	<title>آیت الله ربانی</title>
 	<link href="1styles.css" rel="stylesheet" type="text/css" />
 	<link href="style.less" rel="stylesheet/less" type="text/css" />
 	<script type="text/javascript" src="scripts/less.js">
 	</script>
 	<!--[if IE]>
-		<script type="text/javascript" src="/scripts/html5.js"></script>
+		<script type="text/javascript" src="scripts/html5.js"></script>
 	<![endif]-->
 </head>
 <body>
-	
+	<!-- header -->
+	<header class="cover">
+		<div class="header cover">
+			<a href="#" class="logo">
+				<h1>rabbani</h1>
+			</a>
+			<nav id="main-menu">
+				<ul>
+					<li><a href="#">معرفی شخصیت</a></li>
+					<li><a href="#">خاطرات</a></li>
+					<li><a href="#">گالری</a></li>
+					<li><a href="#">آثار</a></li>
+					<li><a href="#">دروس</a></li>
+					<li><a href="#">پرسش و پاسخ</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="slide">
+			
+		</div>
+	</header>
+	<!-- /header -->
 </body>
 </html>
