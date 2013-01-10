be9e7ec3741aff4365aa530f84465aa52e68e75d
zohreh-z <zahedi.zohreh@gmail.com>
2013-01-10 20:40:39 +0330

    add file header.php

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>آیت الله ربانی</title>
+	<link href="1styles.css" rel="stylesheet" type="text/css" />
+	<link href="style.less" rel="stylesheet/less" type="text/css" />
+	<link type="text/css" rel="stylesheet" href="lightbox.css" />
+	<script type="text/javascript" src="scripts/less.js">
+	</script>
+	<script type="text/javascript" src="scripts/jquery.js"></script>
+	<script type="text/javascript" src="scripts/jquery-ui.js"></script>
+	<script type="text/javascript" src="scripts/lightbox.js"></script>
+	<!--[if IE]>
+		<script type="text/javascript" src="scripts/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- header -->
+	<header class="cover" id="top">
+		<div class="header cover">
+			<a href="./index.php" class="logo">
+				<h1>rabbani</h1>
+			</a>
+			<nav id="main-menu">
+				<ul>
+					<li><a href="./about.php">معرفی شخصیت</a></li>
+					<li><a href="./kotob.php">آثار</a></li>
+					<li><a href="./sertificate.php">مدارک اجتهاد</a></li>
+					<li><a href="#">آلبوم تصاویر</a></li>
+					<li><a href="./question.php">پرسش و پاسخ</a></li>
+					<li><a href="./contact.php">ارتباط با ما</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="slide">
+			<div class="sliderstyle cover">
+				<div id="slider">
+					<div class="content-slide cover"></div>
+					
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- /header -->
+	<!-- containre -->
+	<section class="content-container">
+		<div class="container cover">
+			<h1 class="text-content">
+				به سایت رسمی آیت الله محمد ابراهیم ربانی خوش آمدید
+			</h1>
