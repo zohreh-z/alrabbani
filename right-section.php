0660f699622540e562530034c0e99f6693f96a4d
zohreh-z <zahedi.zohreh@gmail.com>
2013-01-10 20:43:08 +0330

    add files...

---
+++ right-section.php
@@ -0,0 +1,74 @@
+<div class="sidebar-right">
+				<ul class="main-sidbar">
+					<li class="search">
+						<form action="" method="get" id="search">
+							<div style="position:relative;">
+								<label for="s" style="position: absolute; top: 15px; left: 14.8525px; display: inline; z-index: 99;">جستجو</label>
+								<input type="text" value="" onfocus="this.value=''; this.onfocus=null;" name="s" id="s" />
+							</div>
+							<input type="submit" value="" name="submit" />
+						</form>
+					</li>
+					<li>
+						<div class="hadis cover">
+							<div class="top-hadis">
+								<h2>حدیث روز</h2>	
+							</div>
+							<div class="content-hadis">
+								<h3>محیط زیست</h3>
+								<p>حضرت محمد(صلی الله علیه و آله):
+
+المسلمون شرکا فی ثلاث : فی المزاع و کلا وانار
+
+مسلمانان در سه چیز شریکند : آب ، مرتع ، آتش</p>
+								<p>
+									Muslims share in three things: water, pasture, and fire (fuel).
+
+								</p>	
+								<h3>
+									میزان الحکمه ، ح 9634
+								</h3>
+
+								
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="hadis cover">
+							<div class="top-hadis">
+								<h2>دانلود کتب</h2>	
+							</div>
+							<div class="content-hadis pl1" style="padding-right:10px;">
+								<ul class="kotob">
+									<li><a href="#" title="آداب سفر">آداب سفر</a></li>
+									<li><a href="#" title="نور علی نور">نور علی نور</a></li>
+									<li><a href="#" title="شب چراغ">شب چراغ</a></li>
+									<li><a href="#" title="ازدواج و آداب آن">ازدواج و آداب آن</a></li>
+								</ul>
+								
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="hadis cover">
+							<div class="top-hadis">
+								<h2>آمار سایت</h2>	
+							</div>
+							<div class="content-hadis">
+								<script type="text/javascript" src="http://1abzar.ir/abzar/tools/stat/amar-v2.php?color=333333&bg=transparent&kc=888888&kadr=1&amar=odob-g4iezip4hzsvcdz">
+								</script>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="hadis cover">
+							<div class="top-hadis">
+								<!-- <h3>اوقات شرعی</h3> -->	
+							</div>
+							<div class="content-hadis" id="oghat">
+<script type="text/javascript" src="http://1abzar.ir/abzar/tools/azan/v2/?color1=050505&color2=ADADAD&bg=transparent&kc=B3181D&kadr=1&shahr=9-1"></script>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
